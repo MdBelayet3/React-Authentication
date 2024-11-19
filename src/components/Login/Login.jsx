92c2482ee2819106dc1049a596b6f35335e6f54b
Md belayet <mdbelayethossain016@gmail.com>
2024-11-19 18:42:51 +0600

    primary setup complete

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">This is login section</h2>
+        </div>
+    );
+};
+
+export default Login;
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+
 const Root = () => {
     return (
-        <div>
-            
+        <div className="max-w-96 md:max-w-5xl mx-auto">
+            <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 };
